3feb8f859a07450a5a908a718105984950ddc3bb
pvegliante@fvi.edu <pvegliante@fvi.edu>
2017-02-06 19:35:32 -0500

    complete html5-videoplayer

---
+++ main.js
@@ -0,0 +1,21 @@
+var video = $("video")[0];
+var fast = $("#fast")[0];
+var slow = $("#slow")[0];
+var play = $("#play")[0];
+
+
+  // v.playbackRate = 1.5;
+  // v.playbackRate = .5;
+  // v.playbackRate = 1.0;
+
+$("#fast").click(function() {
+  video.playbackRate += .1;
+});
+
+$("#slow").click(function() {
+  video.playbackRate -= .1;
+});
+
+$("#play").click(function() {
+  video.playbackRate = 1.0;
+});
